2c25237f0c76b36f89ec1ddbc48cab846b447b98
unknown <nguyenvanhung.k54@ftu.edu.vn>
2018-01-07 11:26:49 +0700

    Fix condition

--- app.js
+++ app.js
@@ -60,9 +60,6 @@ app.post('/webhook', (req, res) => {
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
-          if (webhook_event.message){
-            timthp(webhook_event.message)
-          }
       } else if (webhook_event.postback) {
         
         handlePostback(sender_psid, webhook_event.postback);
