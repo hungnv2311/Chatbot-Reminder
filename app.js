a0636019b834166f3571ee0242e18a91e3f95688
unknown <nguyenvanhung.k54@ftu.edu.vn>
2018-01-07 23:18:17 +0700

    thử

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ function handleMessage(sender_psid, received_message) {
     // will be added to the body of our request to the Send API
     var response_findclass = {"text": "Tìm lịch học bằng cách nhập tên môn học?"}
     console.log(!userData.response_findclass);
-      if(!userData.response_findclass){
+      if(userData.response_findclass){
           userData.response_findclass = true;
           callSendAPI(sender_psid, response_findclass);
           console.log(userData);
