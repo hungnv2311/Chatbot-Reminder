3aec9e22b1934df3e14ac4ed00ec0ec5a4a437cb
unknown <nguyenvanhung.k54@ftu.edu.vn>
2018-01-07 22:41:36 +0700

    n

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ function handleMessage(sender_psid, received_message) {
     console.log(!userData.response_findclass);
       //!userData.response_findclass
       if(!userData.response_findclass){
-          userData.response_findclass = true;
+          // userData.response_findclass = true;
           callSendAPI(sender_psid, response_findclass);
           console.log(userData.response_findclass);
           console.log(!userData.response_findclass);
@@ -127,7 +127,7 @@ function handleMessage(sender_psid, received_message) {
       else if (userData.response_findclass && !userData.tim_ten_hoc_phan){
         timthp(received_message.text);
         console.log(userData);
-          // userData.tim_ten_hoc_phan = true
+          userData.tim_ten_hoc_phan = true
         }
   }
   else if (received_message.attachments) {
